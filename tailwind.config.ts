c2838ed3dee7381bcb6a5b45a23f2fd5149153f5
devwithmohit <developersmohit@gmail.com>
2025-07-18 12:19:41 +0530

    Implement MongoDB User model with message support

---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx}",
+//     "./components/**/*.{js,ts,jsx,tsx}",
+//     // Add more paths if needed
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Add more paths if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
